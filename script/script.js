861f4c1b5f6f33683739aa87c202fbd1b01ed2ed
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-25 10:36:30 +0700

    Add edit button for ALIGN paragraph

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
 function takeAction() {
   alert("I am taking action!");
 }
+function editText() {
+  var paraAlign = document.getElementById("align-para");
+  var paraKick = document.getElementById("kickstart-para");
+  var editButton = document.getElementById("editButton");
+
+  paraAlign.innerHTML =
+    `<form><textarea name="" id="edited-text" cols="40" rows="3">` +
+    paraKick.innerText +
+    `</textarea>
+  </form>`;
+  editButton.innerHTML = `<button onclick="saveEdit()">Save</button>`;
+}
+function saveEdit() {
+  var paraAlign = document.getElementById("align-para");
+  var editedText = document.getElementById("edited-text").value;
+  var editButton = document.getElementById("editButton");
+  paraAlign.innerHTML = `<p>${editedText}</p>`;
+  editButton.innerHTML = `<button onclick="editText()">Edit</button>`;
+}
